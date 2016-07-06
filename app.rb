abb37a4c864d773f4106043d9fecfa322efc70e1
Caleb Stokka and Sebastian Prokuski <caleb+sebastian@>
2016-07-06 14:51:35 -0700

    sinatra not running

--- app.rb
+++ app.rb
@@ -1,20 +1,33 @@
-require('sinatra')
-require('sinatra/reloader')
-require('./lib/tamagotchi')
-also_reload('lib/**/*.rb')
+require ('sinatra')
+require ('sinatra/reloader')
+require ('./lib/tamagotchi')
+require ('rufus/scheduler')
+also_reload ('lib/**/*.rb')
 
 get('/') do
   erb(:index)
 end
 
 post('/name') do
-  @name = params.fetch('name')
-  @@my_pet = Tamagotchi.new(@name)
-  @food = @@my_pet.food_level()
+  @@name = params.fetch('name')
+  @@my_pet = Tamagotchi.new(@@name)
+  @@food = @@my_pet.food_level()
+  @@sleep = @@my_pet.sleep_level()
+  @@play = @@my_pet.activity_level()
   erb(:index)
 end
 
 get('/eat') do
-  @food = @@my_pet.eat()
+  @@food = @@my_pet.eat()
+  erb(:index)
+end
+
+get('/sleep') do
+  @@sleep = @@my_pet.nap()
+  erb(:index)
+end
+
+get('/play') do
+  @@play = @@my_pet.play()
   erb(:index)
 end
